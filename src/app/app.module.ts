c8877f71606df898f3c543a44456a7420a5f299d
nour-beyrem <53348004+nour-beyrem@users.noreply.github.com>
2021-02-17 08:37:59 +0100

    update

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,11 +4,17 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { FirstComponent } from './first/first.component';
+import { SecondComponent } from './second/second.component';
+import { ThirdComponent } from './third/third.component';
+import { NewComponent } from './new/new.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    FirstComponent
+    FirstComponent,
+    SecondComponent,
+    ThirdComponent,
+    NewComponent
   ],
   imports: [
     BrowserModule,
